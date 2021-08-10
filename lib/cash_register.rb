9415a2e097e3852cd67bc78a66ed702cb02e765b
Ivan Candido <ivan.candido@powerhrg.com>
2021-08-10 10:42:06 -0400

    Completed Assignment

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+require "pry"
 
+class CashRegister
+  attr_accessor :total, :discount, :item_array, :last_transaction
+
+  def initialize(discount = 0)
+    self.item_array = []
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(item_title, price, quantity = 1)
+    counter = 0
+    while counter < quantity do
+      self.item_array << item_title
+      counter += 1
+    end
+    @last_transaction = price * quantity
+    @total += price * quantity
+  end
+
+  def apply_discount
+    @total -= @total * discount / 100
+    discount > 0 ? "After the discount, the total comes to $#{@total}." : "There is no discount to apply."
+  end
+
+  def items
+    self.item_array
+  end
+
+  def void_last_transaction
+    @total -= @last_transaction
+  end
+
+  @total
+end
